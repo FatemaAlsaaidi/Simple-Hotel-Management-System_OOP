8e950ff241e627a7c9aea1cee1f11a8842b8140b
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-13 09:35:51 +0400

    Create a class called Guest with the following: o Auto-property: Name o Auto-property: NationalID

---
+++ Guest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Hotel_Management_System_OOP
+{
+    class Guest
+    {
+        // private fields for Guest properties
+        private string name;
+        private string national_ID;
+
+        // Auto-property for name 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Auto-property for national ID
+        public string National_ID
+        {
+            get { return national_ID; }
+            set 
+            {
+                // Validate national ID format 
+                if (value.Length == 3)
+                {
+                    national_ID = value;
+                }
+                else
+                {
+                    throw new ArgumentException("National ID must be 14 characters long.");
+                }
+            }
+
+        }
+    }
+}
