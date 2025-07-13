75a1d5efc0daec2423f35618db4df66076a7c954
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-13 09:22:18 +0400

    Create a class called Room with the following: o Field: roomNumber (int) o Field: isBooked (bool) o Property to get room number o Property to get whether the room is booked

---
+++ Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Hotel_Management_System_OOP
+{
+    class Room
+    {
+        private int roomNumber = 10;
+        private bool isBooked = false;
+
+        // read-only property for RoomNumber
+        public int RoomNumber
+        {
+            get { return roomNumber; }
+        }
+
+        // property for IsBooked
+        public bool IsBooked
+        {
+            get { return isBooked; }
+        }
+        
+    }
+}
