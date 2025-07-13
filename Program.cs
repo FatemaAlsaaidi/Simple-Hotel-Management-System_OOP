a78f64b9e2953c52723094480a4e21a1e8ac5e8c
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-13 10:40:28 +0400

    In Main method, create one room and one guest, then: o Book the room o Display room status before and after booking

--- Program.cs
+++ Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Simple Hotel Management System!");
+            // Option to book a room or cancel room 
+            Console.WriteLine("Please choose an option:");
+            Console.WriteLine("1. Book a room");
+            Console.WriteLine("2. Cancel a room booking");
+            Console.WriteLine("0. Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice) 
+            {
+                case 1:
+                    // create new guest object
+                    Guest guest = new Guest();
+                    Console.Write("Enter guest name: ");
+                    guest.Name = Console.ReadLine();
+                    Console.Write("Enter guest national ID (3 characters): ");
+                    try
+                    {
+                        guest.National_ID = Console.ReadLine();
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return; // Exit if the national ID is invalid
+                    }
+                    // Display room status
+                    Room room = new Room();
+
+                    Console.WriteLine($"Room before booking : {room.IsBooked}");
+                    room.Book();
+                    Console.WriteLine($"Guest {guest.Name} with National ID {guest.National_ID} is ready to book a room.");
+                    Console.WriteLine($"Room after booking : {room.IsBooked}");
+
+
+                    break;
+                case 2:
+                    Room cancelRoom = new Room();
+                    cancelRoom.Free();
+                    break;
+                case 0:
+                    Console.WriteLine("Exiting the system. Thank you!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
         }
+
+
     }
 }
--- Room.cs
+++ Room.cs
@@ -8,13 +8,14 @@ namespace Simple_Hotel_Management_System_OOP
 {
     class Room
     {
-        private int roomNumber = 10;
+        private int roomNumber=10 ;
         private bool isBooked = false;
 
         // read-only property for RoomNumber
         public int RoomNumber
         {
             get { return roomNumber; }
+            
         }
 
         // property for IsBooked
