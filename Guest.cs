1ea3b19757417642a8baff0fb748a271ea2917d3
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-13 11:36:24 +0400

    static property and used with constructor overloads on Guest class to count for guest number

--- Guest.cs
+++ Guest.cs
@@ -12,7 +12,10 @@ namespace Simple_Hotel_Management_System_OOP
         // private fields for Guest properties
         private string name;
         private string national_ID;
-        static string hotelName = "ABC Hotel"; // Static field for hotel name
+        private static string hotelName = "ABC Hotel"; // Static field for hotel name
+        private static int guestCount = 0; // Static variable to keep track of the number of guests
+        private static int guestNumber; // Static variable to assign a unique guest number
+
 
         // Auto-property for name 
         public string Name
@@ -43,12 +46,16 @@ namespace Simple_Hotel_Management_System_OOP
         // Add constructor overloads to Guest class to initialize data during object creation.
         public Guest(string name, string national_ID, string hotelName)
         {
+            guestCount++; // Increment the guest count for each new guest created
+            guestNumber = guestCount; // Assign a unique guest number based on the count
             Name = name;
             National_ID = national_ID;
             Guest.hotelName = hotelName; // Set the static hotel name
         }
         public Guest()
         {
+            guestCount++; // Increment the guest count for each new guest created
+            guestNumber = guestCount; // Assign a unique guest number based on the count
             // Default constructor initializes name and national ID to empty strings
             Name = string.Empty;
             National_ID = string.Empty;
