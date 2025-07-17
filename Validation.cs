c86cbafd4cd43751f1c7c463a514032af7d013d7
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-17 09:44:56 +0400

    Validates if the input is a valid string

---
+++ Validation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Hotel_Management_System_OOP
+{
+    class Validation
+    {
+
+        // Validates if the input is a valid string 
+        public static bool IsValidString(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input) && input.Length >= 3;
+        }
+
+        
+    }
+}
