bf42f3829f1ebce58c71d68eba1797e47b62425f
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-07-17 15:42:40 +0400

    Authentication to check if ID is exist or not

---
+++ Authentication.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Hotel_Management_System_OOP
+{
+    class Authentication
+    {
+        // ============================ Ckeck if ID Exist ============================
+
+        public static bool CheckUserIDExist(string UserID)
+        {
+
+            //Loop through the list of registered National IDs in guest oblect 
+            foreach (Guest guest in Guest.guest)
+            {
+                // Check if the current guest's National ID matches the provided UserID
+                if (guest.National_ID.Equals(UserID, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true; // User ID exists in the list
+                }
+            }
+
+            return false; // User ID does not exist in the list
+
+        }
+
+
+        // ============================ Ckeck if Password Exist ============================
+        public static bool ExistPassword(string HashPassword)
+        {
+            // Loop through the list of registered password
+            foreach (Guest guest in Guest.guest)
+            {
+                // Check if the current guest's National ID matches the provided UserID
+                if (guest.HashPassword.Equals(HashPassword, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true; // User ID exists in the list
+                }
+            }
+            return false; // User password does not exist in the list
+        }
+
+    }
+}
