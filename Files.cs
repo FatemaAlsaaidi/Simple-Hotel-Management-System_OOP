c21c9263f8dc30a5870ce2c1c88ffa86932a9234
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-19 23:43:06 +0400

    Function to load room data from a file

--- Files.cs
+++ Files.cs
@@ -27,6 +27,33 @@ namespace Simple_Hotel_Management_System_OOP
             }
         }
 
+        // Function to load room data from a file
+        public static void LoadRoomDataFromFile()
+        {
+            // clear list of room before loading data from file
+            Room.rooms.Clear();
+            // Check if the file exists before reading
+            if (System.IO.File.Exists(RoomFilePath))
+            {
+                string[] lines = System.IO.File.ReadAllLines(RoomFilePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+                    if (parts.Length == 4)
+                    {
+                        int roomNumber = int.Parse(parts[0]);
+                        string roomType = parts[1];
+                        double dailyRate = double.Parse(parts[2]);
+                        bool isBooked = bool.Parse(parts[3]);
+                        // 1. Create a new Room object using the overloaded constructor
+                        Room loadedRoom = new Room(roomNumber, roomType, dailyRate, isBooked);
+                        // 2. Add this new Room object to the list
+                        Room.rooms.Add(loadedRoom);
+                    }
+                }
+            }
+        }
+
 
     }
 }
