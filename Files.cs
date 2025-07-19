4de95c9a3694f1f1add5328b6d110de76a41cb74
Fatema Alsaaidi <fatemaalsaaidi116@gmail.com>
2025-07-19 23:12:35 +0400

    create files class + create files for room data, guests data, booking data

---
+++ Files.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Hotel_Management_System_OOP
+{
+    class Files
+    {
+        // Create file to save room data
+        public static string RoomFilePath = "rooms.txt"; // File path for room data
+        // Create file to save guest data
+        public static string GuestFilePath = "guests.txt"; // File path for guest data
+        // Create file to save booking data
+        public static string BookingFilePath = "bookings.txt"; // File path for booking data
+
+
+    }
+}
